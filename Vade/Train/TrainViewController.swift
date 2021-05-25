650b8d634d45b0581620a965b1732c71a863239a
Daria <tokdariatok@gmail.com>
2021-05-26 01:29:59 +0300

    added user location detection

--- Vade/Train/TrainViewController.swift
+++ Vade/Train/TrainViewController.swift
@@ -28,24 +28,59 @@ class TrainViewController: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
+    func checkAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            locationManager.startUpdatingLocation()
+            break
+        case .denied:
+            showAlertLocation(title: "You have banned the use of the location", message: "Do you want to change this?", url: URL(string: UIApplication.openSettingsURLString))
+            break
+        case .restricted:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func showAlertLocation(title: String, message: String?, url: URL?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let settingsAction = UIAlertAction(title: "Settings", style: .default){ alert in
+            if let url = url {
+                UIApplication.shared.open(url)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel , handler: nil )
+        alert.addAction(settingsAction)
+        alert.addAction(cancelAction)
+         
+        present(alert, animated: true, completion: nil)
+    }
+    
     func checkLocationEnable() {
         if CLLocationManager.locationServicesEnabled(){
             setupManager()
+            checkAuthorization()
         } else {
-            let alert = UIAlertController(title: "Geolocation service is disabled!", message: "Want to turn it on?", preferredStyle: .alert)
-            let settingsAction = UIAlertAction(title: "Settings", style: .default){ alert in
-                if let url = URL(string: "App-prefs:LOCATION_SERVICES"){
-                    UIApplication.shared.open(url)
-                }
-            }
-            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel , handler: nil )
-            alert.addAction(settingsAction)
-            alert.addAction(cancelAction)
-             
-            present(alert, animated: true, completion: nil)
+            showAlertLocation(title: "Geolocation service is disabled!", message: "Want to turn it on?", url: URL(string: "App-prefs:LOCATION_SERVICES"))
         }
     }
 }
 extension TrainViewController: CLLocationManagerDelegate {
     
+    //updates coordinates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last?.coordinate {
+             let region = MKCoordinateRegion(center: location, latitudinalMeters: 5000, longitudinalMeters: 5000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+
+    //when user change authorization
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkAuthorization()
+    }
 }
