ee9fed2c64fdb5512ef88e8396f320c3acfc6ae9
Daria <tokdariatok@gmail.com>
2021-06-07 15:14:20 +0300

    add singleton for location manager

---
+++ Vade/Models/LocationManager.swift
@@ -0,0 +1,15 @@
+//
+//  LocationManager.swift
+//  Vade
+//
+//  Created by Daria Tokareva on 03.06.2021.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager {
+  static let shared = CLLocationManager()
+  
+  private init() { }
+}
